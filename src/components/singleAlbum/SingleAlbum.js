f2bb14999c1a0458e2f9f5b1d975746185f32b5f
Matias <mati_d_pinto@hotmail.com>
2021-11-20 16:57:46 -0300

    Album single component (displayed in Home page) & Styles created

---
+++ src/components/singleAlbum/SingleAlbum.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import styles from './Styles';
+
+const SingleAlbum = ({title, username, length}) => {
+    return(
+        <View>
+            <Text style={styles.title}>{title}</Text>
+            <Text>asdasdasd</Text>
+            <Text>22222</Text>
+        </View>
+    )
+};
+
+export default SingleAlbum;
