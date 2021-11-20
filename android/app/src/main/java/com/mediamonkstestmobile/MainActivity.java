263b33edddee97169deb174b0a89aad1ab50f685
Matias <mati_d_pinto@hotmail.com>
2021-11-20 12:33:45 -0300

    react-native-screens requiered configuration added

--- android/app/src/main/java/com/mediamonkstestmobile/MainActivity.java
+++ android/app/src/main/java/com/mediamonkstestmobile/MainActivity.java
@@ -1,8 +1,14 @@
 package com.mediamonkstestmobile;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
@@ -12,4 +18,6 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "mediamonksTestMobile";
   }
+
+
 }
