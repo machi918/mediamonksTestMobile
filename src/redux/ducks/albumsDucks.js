ac3a12d6de4303925aea685f10c86159783c634b
Matias <mati_d_pinto@hotmail.com>
2021-11-20 12:48:25 -0300

    albumsDucks added

---
+++ src/redux/ducks/albumsDucks.js
@@ -0,0 +1,63 @@
+import { getAllAlbums } from "../../controllers/AlbumsController";
+
+//Constants
+const initialData = {
+
+    albumsList: [],
+    isLoading: false
+
+};
+
+//----------------------------------------------------
+
+
+//Types
+const GET_ALBUM_SUCCESS = 'GET_ALBUM_SUCCESS';
+const GET_ALBUM_LOADING = 'GET_ALBUM_LOADING';
+//----------------------------------------------------
+
+//Reducer
+export default function albumsReducer(state = initialData, action){
+    switch (action.type) {
+        case GET_ALBUM_SUCCESS:
+            return {...state, albumsList: action.payload, isLoading: action.isLoading};
+        case GET_ALBUM_LOADING:
+            return {...state, isLoading: action.isLoading};
+        default:
+            return state;
+    };
+};
+//----------------------------------------------------
+            
+//Actions
+export const fetchAlbums = () => async (dispatch, getState) => {
+
+    console.log('Into fetchAlbums ACTION');
+    dispatch({type: GET_ALBUM_LOADING, isLoading: true})
+    try {
+
+        await getAllAlbums().then(data => {
+            console.log('DATA: ');
+            console.log(data);
+            dispatch({type: GET_ALBUM_SUCCESS, payload: data, isLoading: false})
+        })
+        
+    } catch (error) {
+        console.log(error);
+    } finally{
+        dispatch({type: GET_ALBUM_LOADING, isLoading: false})
+    }
+};
+
+   
+//----------------------------------------------------
+
+
+
+
+
+
+
+
+
+
