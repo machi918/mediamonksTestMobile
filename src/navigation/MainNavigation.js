a0e8812c99fab116773beac09901e49a771f77ac
Matias <mati_d_pinto@hotmail.com>
2021-11-20 17:02:03 -0300

    New MainNavigation structure created

---
+++ src/navigation/MainNavigation.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../pages/home/Home";
+import Album from "../pages/album/Album";
+import PhotoPage from "../pages/photo/PhotoPage";
+
+
+const MainNavigation = ({navigation}) => {
+
+    const Stack = createNativeStackNavigator();
+
+    return(
+        <NavigationContainer>
+
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={Home} options={{ headerShown: true }} />
+                <Stack.Screen name="Album" component={Album} options={{ headerShown: true }} />
+                <Stack.Screen name="PhotoPage" component={PhotoPage} options={{ headerShown: true }} />
+                {/* <Stack.Screen name="Home" component={Home} options={{ headerShown: true }} /> */}
+
+            </Stack.Navigator>
+        
+        </NavigationContainer>
+
+    )
+
+};
+
+export default MainNavigation;
