6d8699995ed6ffc33d4167f03b5ac2a0c1c53fc2
Matias <mati_d_pinto@hotmail.com>
2021-11-20 21:46:53 -0300

    ImageBackground and props re-style

--- src/components/singleAlbum/SingleAlbum.js
+++ src/components/singleAlbum/SingleAlbum.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, ImageBackground } from 'react-native';
 import styles from './Styles';
 
-const SingleAlbum = ({title, username, length}) => {
+const SingleAlbum = ({title, username, length, src}) => {
     return(
-        <View>
-            <Text style={styles.title}>{title}</Text>
-            <Text>asdasdasd</Text>
-            <Text>22222</Text>
-        </View>
+        <ImageBackground
+        source={{uri :src }}
+        style={{width: '100%', height: '100%'}}
+        >
+            <View style={{justifyContent: 'flex-end', width: '100%', height: '100%', padding: 10}}>
+                <Text style={styles.title}>{title}</Text>
+                <Text>asdasdasd</Text>
+                <Text>22222</Text>
+            </View>
+        </ImageBackground>
     )
 };
 
