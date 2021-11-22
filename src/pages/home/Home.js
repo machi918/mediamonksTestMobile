74d5b25de9369956cc34f125e6944b36350b15a8
Matias <mati_d_pinto@hotmail.com>
2021-11-22 17:21:03 -0300

    UIDarkMode integration added. Improved code readability

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,17 +1,16 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { SafeAreaView, Text, FlatList, View, TouchableOpacity, Dimensions, Button } from 'react-native';
 import Carousel from 'react-native-snap-carousel';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import styles from './Styles';
+import colors from '../../constants/colors';
 import Loading from '../../components/loading/Loading';
-
 import SingleAlbum from '../../components/singleAlbum/SingleAlbum';
 
-//Hooks react redux
+//Hooks react redux & Actions
 import { useDispatch, useSelector } from 'react-redux';
-//Actions
 import { fetchAlbums } from '../../redux/ducks/albumsDucks';
+
 
 
 const {height, width} = Dimensions.get('window');
@@ -27,34 +26,39 @@ const Home = ({navigation}) => {
     const albumsState = useSelector(store => store.albums);
     const albumsStateList= useSelector(store => store.albums.albumsList);
     const photosStateList= useSelector(store => store.albums.photosList);
+    const isDarkMode = useSelector(store => store.ui.isDarkMode );
     
     useEffect(() => {
         const fetch = async () => {
             await dispatch(fetchAlbums());
         };
         fetch();
-        console.log('USEEFFECT DE HOME');
-    }, [])
+    }, []);
 
     const handleClick = async () => {
         try {
-            await AsyncStorage.removeItem('albums');
-            await AsyncStorage.removeItem('photos');
-          } catch(e) {
+            // await AsyncStorage.removeItem('albums');
+            // await AsyncStorage.removeItem('photos');
+        } catch(e) {
             // remove error
             console.log('ME ROMPI EN HOME');
-          }
+        }
+    }
+
+    const handleOnClickAlbum = (item) => {
+        navigation.navigate('Album', { title: item.item.title, idAlbum: item.item.id });
     }
 
     return(
-        <SafeAreaView style={styles.container}>
+        <SafeAreaView style={[styles.container, isDarkMode ? {backgroundColor: colors.primaryDark} : {backgroundColor: colors.primary}]}>
             {
-                albumsState.isLoading ? <Loading/> : 
-                <View style={{height: '100%', width: '100%'}}>
-                    <Button title={'asdasdasdas'} onPress={() => handleClick()}/>
+                albumsState.isLoading ? <Loading uiDark={isDarkMode}/> : 
 
-                    <Text style={{marginTop: 15, marginHorizontal: 10, color: 'black'}}>My First 5 Albums</Text>
-                    <View style={{width: '100%', height: 225, alignItems: 'center', justifyContent: 'center'}}>
+                <View style={{height: '100%', width: '100%'}}>
+                    <Button title={'Reset Storage'} onPress={() => handleClick()}/>
+
+                    <Text style={[styles.textAlbumTitle, isDarkMode ? {color:colors.textColorDark}:{}]}>My First 5 Albums</Text>
+                    <View style={[styles.carrouselContainer, isDarkMode ? {backgroundColor: colors.primaryDark} : {backgroundColor: colors.primary}]}>
                         <Carousel
                         ref={carouselRef}
                         layout={'default'}
@@ -62,14 +66,14 @@ const Home = ({navigation}) => {
                         sliderWidth={width}
                         itemWidth = {width*0.8}
                         renderItem={(item) => 
-                            <TouchableOpacity onPress={() => navigation.navigate('Album', { title: item.item.title, idAlbum: item.item.id })} style={styles.albumComponentMain}>
-                                        <SingleAlbum title={item.item.title} src={photosStateList.filter(itemList => itemList.albumId == item.item.id)[0].thumbnailUrl}/>
+                            <TouchableOpacity onPress={() => handleOnClickAlbum(item)} style={styles.albumComponentMain}>
+                                <SingleAlbum title={item.item.title} src={photosStateList.filter(itemList => itemList.albumId == item.item.id)[0].thumbnailUrl}/>
                             </TouchableOpacity>
                         }
                         />
                     </View>
 
-                    <Text style={{marginBottom: 5, marginHorizontal: 10, color: 'black'}}>All Albums</Text>
+                    <Text style={[styles.textAlbumTitle, isDarkMode ? {color:colors.textColorDark}:{}]}>All Albums</Text>
 
                     <FlatList 
                     style={{height: '70%'}}
@@ -80,9 +84,9 @@ const Home = ({navigation}) => {
                     contentContainerStyle={{ alignItems:'center' }}
                     columnWrapperStyle={{flexWrap: 'wrap'}}
                     renderItem={item => 
-                            <TouchableOpacity onPress={() => navigation.navigate('Album', { title: item.item.title, idAlbum: item.item.id })} style={styles.albumComponent}>
-                                <SingleAlbum title={item.item.title} src={photosStateList.filter(itemList => itemList.albumId == item.item.id)[0].thumbnailUrl} />
-                            </TouchableOpacity>
+                        <TouchableOpacity onPress={() => handleOnClickAlbum(item)} style={styles.albumComponent}>
+                            <SingleAlbum title={item.item.title} src={photosStateList.filter(itemList => itemList.albumId == item.item.id)[0].thumbnailUrl} />
+                        </TouchableOpacity>
                     }
                     />
 
