5b0304aae5a348d154237d1ad7f2c17e9b8381a0
Matias <mati_d_pinto@hotmail.com>
2021-11-22 17:09:50 -0300

    UITheme integration added

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import { View, Text, ActivityIndicator, Image } from 'react-native';
+import { View, ActivityIndicator, Image } from 'react-native';
 
 import styles from './Styles';
+import colors from '../../constants/colors';
 
-const Loading = () => {
-
+const Loading = ({uiDark}) => {
     return(
-
-        <View style={styles.container}>
-            <Image source={require('../../assets/images/mmLogo.png')} style={styles.imgLogo} resizeMode={'contain'} />
-            <ActivityIndicator size={'large'} color={'black'}/>
-
-
+        <View style={[styles.container, uiDark ? {backgroundColor: colors.primaryDark} : {backgroundColor: colors.primary}]}>
+            <Image source={require('../../assets/images/mmLogo.png')} style={[styles.imgLogo, uiDark ? {tintColor: colors.textColorDark} : {tintColor: colors.black}]} resizeMode={'contain'} />
+            <ActivityIndicator size={'large'} color={uiDark ? colors.textColorDark : colors.black}/>
         </View>
-
     )
-
-
 };
 
 export default Loading;
