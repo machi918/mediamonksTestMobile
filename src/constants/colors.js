fb7c896d4bd1c112423121b999ae4f15487b8ecd
Matias <mati_d_pinto@hotmail.com>
2021-11-22 17:11:44 -0300

    Color constants file created

---
+++ src/constants/colors.js
@@ -0,0 +1,22 @@
+const colors = {
+
+    primary: "#DFDEDE",
+    primaryDark: "#121212",
+    secondary: "",
+    secondaryDark: "",
+    warning: "#8C0900",
+    // warning: "#BF422C",
+
+    textColor: "#000000",
+    textColorDark: "rgba(255, 255, 255, 0.8)",
+
+    transparentBlack: "rgba(0, 0, 0, 0.4)",
+    transparentWhite: "rgba(255, 255, 255, 0.2)",
+    inactiveTint: "#BEBEBE",
+
+    white: "#FFFFFF",
+    black: "#000000"
+
+};
+
+export default colors;
