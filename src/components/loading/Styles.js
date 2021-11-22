739ebcf033a72edebceb933feea56ebda30c733d
Matias <mati_d_pinto@hotmail.com>
2021-11-22 17:08:08 -0300

    Global color constants supplied

--- src/components/loading/Styles.js
+++ src/components/loading/Styles.js
@@ -1,22 +1,21 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../constants/colors';
 
 const styles = StyleSheet.create({
     container:{
         width: '100%',
         height: '100%',
+
         justifyContent:'center',
         alignItems:'center'
-
     },
     imgLogo:{
-        tintColor: 'black',
+        tintColor: colors.black,
         marginVertical: 20,
 
         width: '70%',
         height: 100,
     }
-
-
 });
 
 export default styles;
