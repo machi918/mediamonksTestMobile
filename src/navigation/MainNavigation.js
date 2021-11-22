0402636bd5f076734330a48ce3d4b34bf65c6d95
Matias <mati_d_pinto@hotmail.com>
2021-11-22 17:16:48 -0300

    UITheme handler. Dispatch's the user's phone theme

--- src/navigation/MainNavigation.js
+++ src/navigation/MainNavigation.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useCallback } from "react";
+import { Appearance } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../pages/home/Home";
@@ -6,14 +7,37 @@ import Album from "../pages/album/Album";
 import PhotoPage from "../pages/photo/PhotoPage";
 
 
+//Hooks react redux
+import { useDispatch, useSelector } from 'react-redux';
+//Actions
+import { darkTheme } from "../redux/ducks/uiDucks";
+
 const MainNavigation = ({navigation}) => {
 
     const Stack = createNativeStackNavigator();
 
+    // declaramos displach para llamar a la acción o acciones
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        Appearance.addChangeListener(themeChangeListener);
+        const initialTheme = async () => {
+            await dispatch(darkTheme(Appearance.getColorScheme()))
+        }
+        initialTheme()
+        return () => {
+            Appearance.addChangeListener().remove()
+        }
+    }, []);
+
+    const themeChangeListener = useCallback(() => {
+        dispatch(darkTheme(Appearance.getColorScheme()))
+    }, []);
+
     return(
         <NavigationContainer>
 
-            <Stack.Navigator>
+            <Stack.Navigator initialRouteName={'Home'}>
                 <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
                 <Stack.Screen name="Album" component={Album} options={{ headerShown: true }} />
                 <Stack.Screen name="PhotoPage" component={PhotoPage} options={{ headerShown: true }} />
