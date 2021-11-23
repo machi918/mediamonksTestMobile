c2dd83c8872e8373dd422e70808ba168eb03d70a
Matias <mati_d_pinto@hotmail.com>
2021-11-23 00:34:06 -0300

    Description added

--- src/controllers/AlbumsController.js
+++ src/controllers/AlbumsController.js
@@ -1,5 +1,9 @@
 import { allAlbums } from "../services/Apiroutes";
 
+/**
+ * 
+ * @description Gets all available albums. Returns status code if not succeed.
+ */
 export const getAllAlbums = async () => {
     const response = await allAlbums();
     if(response.status === 200){
