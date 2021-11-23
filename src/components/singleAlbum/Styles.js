31c149d7924558bd371b02d474f3cbcd1522e826
Matias <mati_d_pinto@hotmail.com>
2021-11-22 21:52:54 -0300

    Global color constants supplied

--- src/components/singleAlbum/Styles.js
+++ src/components/singleAlbum/Styles.js
@@ -1,10 +1,19 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../constants/colors';
 
 const styles = StyleSheet.create({
     title:{
         overflow: 'hidden',
-        color: 'white'
+        color: 'white',
     },
+
+    textContainer:{
+        justifyContent: 'center',
+        width: '100%',
+        height: '40%',
+        padding: 10,
+        backgroundColor: colors.transparentBlack,
+    }
 
 });
 
