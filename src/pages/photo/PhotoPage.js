e82a784c7a6987c9b623ddd71273dfc58016e11b
Matias <mati_d_pinto@hotmail.com>
2021-11-22 21:56:59 -0300

    Animations added. Background color changes. Header fades away

--- src/pages/photo/PhotoPage.js
+++ src/pages/photo/PhotoPage.js
@@ -1,19 +1,103 @@
-import React from 'react';
-import { SafeAreaView, Text, Button, Image } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { SafeAreaView, Text, Button, Image, TouchableWithoutFeedback, TouchableOpacity,Animated, View   } from 'react-native';
 
 import styles from './Styles';
 
+//Hooks react redux
+import { useDispatch, useSelector } from 'react-redux';
+import colors from '../../constants/colors';
+
 
 const PhotoPage = ({navigation, route}) => {
-
     const {title, thumbnailUrl, url, id} = route.params;
 
+    //Get the state from store
+    const isDarkMode = useSelector(store => store.ui.isDarkMode );
+
+
+    const fadeAnim = useRef(new Animated.Value(1)).current; //Opacity reference Animation
+    const fadeBackground = useRef(new Animated.Value(1)).current; //Background Color w/ extrapolation reference Animation
+
+    const [topLayer,setTopLayer] = useState(true); //If true, header's opacity is 1
+    const [topBackLayer,setBackLayer] = useState(true); //If true, background page color, defaults to UI theme.
+    const [touched, setTouched] = useState(true); //If true, back button is available
+
+    useEffect(() => {
+        
+        const timer = setInterval(()=>{
+            handleTouchDisplay();
+            clearInterval(timer);
+            setTouched(false);
+        },1250);
+
+        return () => {
+            clearInterval(timer);
+        }
+    }, []);
+
+    const handleTouchDisplay = ()=>{
+
+        Animated.parallel([
+            Animated.timing(fadeAnim, {
+                toValue: topLayer ? 0 : 1,
+                duration: 250,
+                useNativeDriver: true
+            }).start(({ finished })=>{
+            }),
+            Animated.timing(fadeBackground, {
+                toValue: topBackLayer ? 0 : 1,
+                duration: 250,
+                useNativeDriver: false
+            }).start(({ finished })=>{
+            }),
+        ]).start(()=>{
+            setTopLayer(!topLayer);
+            setTouched(!touched);
+            setBackLayer(!topBackLayer);
+        });
+    }
+
+    const boxInterpolation =  fadeBackground.interpolate({
+        inputRange: [0, 1],
+        outputRange:[ colors.black, colors.white]
+      })
+
+    const animatedStyle = {
+        backgroundColor: boxInterpolation
+    }
+
+    const handleGoBack = () => {
+        if(touched){
+            navigation.pop();
+        }
+    }
+
+    //UI Renders------------------------------------------------------
+
+    const topLayerRender = () => {
+        return(
+            <Animated.View
+            style={{width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, zIndex: 100, opacity: fadeAnim}}
+            >
+            <View style={[styles.header, isDarkMode ? {backgroundColor: colors.primaryDark}: {backgroundColor: colors.transparentBlack}]}>
+                <TouchableOpacity style={styles.backButton} onPress={()=>handleGoBack()}>
+                    <Text>BACK</Text>
+                </TouchableOpacity>
+                <Text style={styles.headerText}>{title}</Text>
+            </View>
+        </Animated.View>
+        )
+    };
+    
     return(
-        <SafeAreaView>
-            <Text>PhotoPage: {title} </Text>
-            <Image source={{uri: url}} style={{minWidth:'100%', minHeight: 600, resizeMode:'contain'}} />
-            {/* <Button onPress={()=> navigation.navigate('Album')} title={"Navegar ALBUM"}/> */}
-        </SafeAreaView>
+        <TouchableWithoutFeedback onPress={()=>handleTouchDisplay()} style={{backgroundColor: 'red', width:'100%', height: '100%'}}>
+            <Animated.View style={[{...styles.container, ...animatedStyle}, isDarkMode ? {backgroundColor: colors.primaryDark}: {}]}>
+                    <View>
+                        <Image source={{uri: url}} style={styles.img} />
+                    </View>
+                    {topLayerRender()}
+            </Animated.View>
+        </TouchableWithoutFeedback>
     );
 
 };
