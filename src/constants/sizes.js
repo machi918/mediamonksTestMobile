a5e5018f484a1ad46c86cd34e39c064c559c0790
Matias <mati_d_pinto@hotmail.com>
2021-11-23 10:47:24 -0300

    Sizes file with constants created

---
+++ src/constants/sizes.js
@@ -0,0 +1,52 @@
+const sizes = {
+
+    //General screen sizes
+    screen_small_max_height: 700,
+    screen_small_max_width: 370,
+    screen_regular_max_height: 950,
+    screen_regular_max_width: 650,
+    screen_big_max_height: 2000,
+    screen_big_max_width: 1000,
+    //---------------------------------------------------
+
+    //MainCarousel sizes
+    mainCarousel_small_height: 200,
+    mainCarousel_small_width: '100%',
+    mainCarousel_regular_height: 225,
+    mainCarousel_regular_width: '100%',
+    mainCarousel_big_height: 250,
+    mainCarousel_big_width: '100%',
+    //---------------------------------------------------
+
+    //Main SingleAlbum sizes
+    mainSingleAlbum_small_height: '90%',
+    mainSingleAlbum_small_width: 300,
+    mainSingleAlbum_regular_height: '90%',
+    mainSingleAlbum_regular_width: 300,
+    mainSingleAlbum_big_height: '90%',
+    mainSingleAlbum_big_width: 350,
+    //---------------------------------------------------
+
+    //SingleAlbum sizes
+    singleAlbum_small_height: 150,
+    singleAlbum_small_width: 150,
+    singleAlbum_regular_height: 175,
+    singleAlbum_regular_width: 175,
+    singleAlbum_big_height: 200,
+    singleAlbum_big_width: 200,
+    //---------------------------------------------------
+
+    //Header
+    header_small_height: 75,
+    header_regular_height: 80,
+    header_big_height: 90,
+    //---------------------------------------------------
+
+    //BackButton Container
+    back_button_height: 50,
+    back_button_width: 50,
+    //---------------------------------------------------
+
+};
+
+export default sizes;
