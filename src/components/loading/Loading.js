4dc568d6e2e2a8b8390c340f6fa794c785a49662
Matias <mati_d_pinto@hotmail.com>
2021-11-23 16:07:04 -0300

    LightTheme logo & activity indicator colors changed to secondary

--- src/components/loading/Loading.js
+++ src/components/loading/Loading.js
@@ -12,8 +12,8 @@ import colors from '../../constants/colors';
 const Loading = ({uiDark}) => {
     return(
         <View style={[styles.container, uiDark ? {backgroundColor: colors.primaryDark} : {backgroundColor: colors.primary}]}>
-            <Image source={require('../../assets/images/mmLogo.png')} style={[styles.imgLogo, uiDark ? {tintColor: colors.textColorDark} : {tintColor: colors.black}]} resizeMode={'contain'} />
-            <ActivityIndicator size={'large'} color={uiDark ? colors.textColorDark : colors.black}/>
+            <Image source={require('../../assets/images/mmLogo.png')} style={[styles.imgLogo, uiDark ? {tintColor: colors.textColorDark} : {tintColor: colors.secondary}]} resizeMode={'contain'} />
+            <ActivityIndicator size={'large'} color={uiDark ? colors.textColorDark : colors.secondary}/>
         </View>
     )
 };
