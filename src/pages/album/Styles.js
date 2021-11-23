3b5066c8deb642256c7fcea153e0192a4c26b3a6
Matias <mati_d_pinto@hotmail.com>
2021-11-22 23:09:33 -0300

    Styles added to Album Page

--- src/pages/album/Styles.js
+++ src/pages/album/Styles.js
@@ -1,11 +1,42 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../constants/colors';
 
 const styles = StyleSheet.create({
     container:{
         width:'100%',
-        height: '100%'
-    }
+        height: '100%',
 
+        alignItems: 'center'
+    },
+
+    header:{
+        height: 75,
+        width: '100%',
+
+        backgroundColor: colors.transparentBlack,
+        borderBottomRightRadius: 25,
+        borderBottomLeftRadius: 25,
+
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+
+        position: 'absolute',
+        zIndex: 100,
+
+        overflow: 'scroll'
+    },
+    textHeader: {
+        width: '70%',
+        color: colors.white,
+        fontSize: 20,
+        textAlign: 'center'
+    },
+    backButton:{
+        height: 50,
+        width: 50,
+        backgroundColor: colors.transparentWhite
+    }
 });
 
 export default styles;
